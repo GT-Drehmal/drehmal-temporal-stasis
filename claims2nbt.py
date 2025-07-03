d3d0bf34106a1b9757c374c65d77b447bd684bfa
Cynthia7979 <cynthiawang7979@hotmail.com>
2025-07-03 02:31:24 -0400

    Add UUID & output dir, argument validation, MORE docstrings, and remove old parse-to-location code

--- claims2nbt.py
+++ claims2nbt.py
@@ -1,46 +1,53 @@
 import argparse, csv, os, json, sys
-from typing import Literal
+from typing import Sequence
 from nbtlib import File, Compound, List, Int, Long, String
 
 
-def main(args=None):
+def main(args: Sequence[str] | None = None):
+    """Initializes argument parser and parses arguments to then pass to `claims2nbt`.
+
+    Args:
+        args (Sequence[str], optional):
+            Custom arguments to use when parsing.
+            Defaults to `sys.argv[1:]` (fallback of `parse_args`).
+    """
     parser = argparse.ArgumentParser(
         description="Convert per-chunk claims data into OPaC-compatible .nbt files."
     )
     parser.add_argument('claims', type=str, help='A CSV file that contains the following headers: Location, Chunk X, Chunk Z, and Dimensions.')
-    parser.add_argument('--mapfiledir', type=str, default='./', help='Path to store the UUID mapping file. Defaults to CWD')
+    parser.add_argument('--uuid', type=str, default='00000000-0000-0000-0000-000000000000', help='UUID of the user that the server claims will belong to. Defaults to Server (00000000-0000-0000-0000-000000000000)')
+    parser.add_argument('-d', '--dir', target='path', type=str, default='./', help='Directory to create player-claims and player-configs data in. Defaults to CWD.')
     parsed_args = parser.parse_args(args)
 
-    ret_code = claims2nbt(parsed_args.claims, parsed_args.mapfiledir)
+    if validate_args(parsed_args) != 0:
+        exit(-1)
+
+    ret_code = claims2nbt(parsed_args.claims, parsed_args.uuid, parsed_args.path)
     exit(ret_code)
 
-def get_locations(csv_path: str) -> dict[str, list[tuple[int, int, str]]] | Literal[-1]:
-    if not os.path.exists(csv_path):
-        print(f'{csv_path} does not exist.')
-        return -1
-    if not csv_path.endswith('.csv'):
-        print(f'{csv_path} is not a CSV file.')
-        return -1
-    print(f'Using {csv_path}.')
 
-    locations: dict[str, list[tuple[int, int, str]]] = {}
-    with open(csv_path, newline='') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            if not all(h in row.keys() for h in ("Location", "Chunk X", "Chunk Z", "Dimension")):
-                print(f'The supplied CSV file {csv_path} is malformed.')
-                return -1
-            loc: str = row["Location"]
-            x = int(row["Chunk X"])
-            z = int(row["Chunk Z"])
-            dimension: str = row["Dimension"]
-            locations.setdefault(loc, []).append((x, z, dimension))
+def validate_args(parsed_args: argparse.Namespace):
+    """Confirm that the given command-line arguments are valid.
 
-        if reader.line_num == 0:
-            print('Empty CSV file.')
-            return -1
-        
-    return locations
+    - The CSV file passed for the positional argument `claims` must exist and end in `'.csv'`.
+    - The UUID passed with `--uuid` option must have the format of a Minecraft UUID.
+
+    Args:
+        parsed_args (argparse.Namespace): Argument namespace produced by `argpase.parse_args()`
+
+    Returns:
+        err_code (int): If `0`, arguments are valid. Otherwise, one of the checks failed and the program should be aborted.
+    """
+    if not os.path.exists(parsed_args.claims):
+        print(f'{parsed_args.claims} does not exist.')
+        return -1
+    if not parsed_args.claims.endswith('.csv'):
+        print(f'{parsed_args.claims} is not a CSV file.')
+        return -1
+    if len(parsed_args.uuid) != 36 or parsed_args.count('-') != 4 or not all(l.isalnum() or l == '-' for l in parsed_args.uuid):
+        print(f'Invalid UUID {parsed_args.uuid}.')
+        return -1
+    return 0
 
 
 def parse_dimensions(csv_path: str):
@@ -48,16 +55,11 @@ def parse_dimensions(csv_path: str):
 
     Args:
         csv_path (str): A CSV file that contains non-empty columns "Location", "Chunk X", "Chunk Z", and "Dimension".
+            Assumes that the path exists and is a .csv file.
 
     Returns:
         dimensions (dict): Chunks claimed in each dimension. Looks like `{dimension: [(location, x, z), ...], ...}`
     """
-    if not os.path.exists(csv_path):
-        print(f'{csv_path} does not exist.')
-        return -1
-    if not csv_path.endswith('.csv'):
-        print(f'{csv_path} is not a CSV file.')
-        return -1
     print(f'Using {csv_path}.')
 
     dimensions: dict[str, list[tuple[str, int, int]]] = {}
@@ -102,25 +104,35 @@ def legalize_name(name: str) -> str:
     return legalized_name
 
 
-def claims2nbt(csv_path: str, mapfiledir: str) -> int:
-    claims_dir = "player-claims"
-    os.makedirs(claims_dir, exist_ok=True)
-    subconfigs_dir = os.path.join('player-configs', 'sub-configs', '00000000-0000-0000-0000-000000000000/')
-    os.makedirs(subconfigs_dir, exist_ok=True)
+def claims2nbt(csv_path: str, uuid: str, root_dir: str) -> int:
+    """Generate NBT and toml files for chunk claims specified in the CSV file.
+
+    Two directories `player-claims` and `player-configs` will be created under `root_dir`; any missing directories in between will be created automatically.
+
+    All chunks specified in the CSV will be claimed by the player with the `uuid`.
+    Each location will be claimed using a different sub-config with the area name set to the location's name, truncated if necessary (~100).
+    No other options are touched by sub-configs; common server configs are to be managed manually with the server's `main` config.
     
+    Sub-configs are named using the location's name in an OPAC-compatible format. See `legalize_name`.
+
+    Args:
+        csv_path (str): Path to the CSV file.
+            Must be well-formed and contain the columns "Location", "Chunk X", "Chunk Z", and "Dimension".
+        uuid (str): The player or "player" that will own the claimed chunks.
+        root_dir (str): Directory to store generated files in.
+
+    Returns:
+        ret_code (int): If 0, files have been created successfully. Otherwise, an error has occurred.
+    """
+    claims_dir = os.path.join(root_dir, "player-claims")
+    os.makedirs(claims_dir, exist_ok=True)
+    subconfigs_dir = os.path.join(root_dir ,'player-configs', 'sub-configs', f'{uuid}/')
+    os.makedirs(subconfigs_dir, exist_ok=True)
+
     # parse csv
-    # locations = get_locations(csv_path)
-    # if locations == -1:
-    #     return -1
     dimensions = parse_dimensions(csv_path)
     if dimensions == -1:
         return -1
-
-    # load and write default config for server claims
-    with open('protection-disabled.toml', 'r', encoding='utf-8') as f:
-        config_content = f.read()
-    with open(os.path.join("player-configs", '00000000-0000-0000-0000-000000000000.toml'), 'w', encoding='utf-8') as f:
-        f.write(config_content)
 
     # Generate location name to subconfig mapping on-the-fly, and create the corresponding subconfig file.
     loc_to_index: dict[str, int] = {}
@@ -150,7 +162,8 @@ def claims2nbt(csv_path: str, mapfiledir: str) -> int:
                     "z": Int(z)
                 })
             )
-        # We do two passes for simplicity's sake
+        # We do two passes because NBT Lists cannot append items that are not the End tag
+        # thus making it impossible to add positions to the List in the pass above.
         claims = []
         for loc in loc_chunks:
             claims.append(Compound({
@@ -169,11 +182,10 @@ def claims2nbt(csv_path: str, mapfiledir: str) -> int:
         "username": String("Server"),
         "dimensions": Compound(dimension_compounds),
     })
-    # write nbt file
     nbt_file = File(nbt_data)
-    nbt_file.save(os.path.join(claims_dir, "00000000-0000-0000-0000-000000000000.nbt"))
+    nbt_file.save(os.path.join(claims_dir, f"{uuid}.nbt"))
 
-    print(f"Claims written to 00000000-0000-0000-0000-000000000000.nbt")
+    print(f"Claims written to {uuid}.nbt")
 
     return 0
 
@@ -188,11 +200,6 @@ if __name__ == "__main__":
     #   This value is what my user file was set to, so I am reusing it assuming it won't affect anything.
     #   The value *could* be a timestamp of sorts, but it doesn't track with epoch, gametime (ticks since world creation), or any variation of these two. 
 
-    # d) 'state: Compound({forceloaded: Int(0), subConfigIndex: Int(-1)})'
-    #   State compound is hardcoded to these for now.
-    #   Will need to update later when we configure the correct configs for server-factions.
-
     # TODO:
     # Write to-do section lol
-    # Add correct error throws/behaviors if csv parse doesn't find correct headers
     # Consider adding optional arg for player-claims filepath to write directly to world directory
